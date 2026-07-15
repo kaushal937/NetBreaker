a02353e8298e2753a8bb72363b4ef941c0daca3d
Kaushal <kaushalprakash1166@gmail.com>
2026-07-15 23:19:31 +0530

    improvement in rate limiting

--- src/middlewares/ratelimiting/ratelimiting.ts
+++ src/middlewares/ratelimiting/ratelimiting.ts
@@ -1,8 +1,8 @@
-import RequestRateModule from '../stats/requestRateCounter'
+import {rate} from '../stats/requestRateCounter'
 
 function limitRateTo(ratelimit: number){
     return (req: any, res: any, next: any)=>{
-        if(RequestRateModule.rate > ratelimit){
+        if(rate > ratelimit){
             res.status(429).end()
         }else{
             next()
